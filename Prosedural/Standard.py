331bf49bf6df7190a81ac09705f6ab262e18a7f3
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2021-03-15 15:41:27 +0700

    Menambah Latihan Kuliah terbaru

---
+++ Prosedural/Standard.py
@@ -0,0 +1,11 @@
+# Bayu Samudra (16520420) - 15 Maret 2021
+# PROGRAM Standar
+# Spesifikasi
+
+# KAMUS
+#   iniVariabel : integer
+
+# DDefinisi, spek, realisasi fungsi
+
+# ALGORTIMA
+print("Halo, Dunia")
